927051cf9ebcf92d2b1d78393a6607066e1852b8
Swati Singh <pariswati1103@gmail.com>
2024-06-11 16:00:29 +0530

    tansaction

--- src/pages/Admin-Transaction.jsx
+++ src/pages/Admin-Transaction.jsx
@@ -3,7 +3,7 @@ import { AllTransactionFinancial } from "./AllTransactionFinancial";
 export const AdminTransaction = () => {
   return (
     <div>
-      <p className="text-6xl font-bold text- white my-4"> Admin Transactions Date</p>
+      <p className="text-6xl font-bold text- white my-4"> Admin Transactions Data</p>
       <div className="text-blue-800 bg-slate-200">
         <AllTransactionFinancial />
       </div>
